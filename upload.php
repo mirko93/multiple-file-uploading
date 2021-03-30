5a4fb3637f94e23c6733c283b508c2e4bec9d0e9
Mirko Skopljak <45762923+mirko93@users.noreply.github.com>
2021-03-30 13:26:19 +0200

    created script for multipled uploaded file

--- upload.php
+++ upload.php
@@ -1,2 +1,51 @@
 <?php
 
+if (!empty($_FILES['files']['name'][0])) {
+    
+    $files = $_FILES['files'];
+
+    $uploaded = [];
+    $failed = [];
+    $allowed = ['txt', 'jpg', 'pdf'];
+
+    foreach ($files['name'] as $position => $fileName) {
+        $file_tmp = $files['tmp_name'][$position];
+        $file_size = $files['size'][$position];
+        $file_error = $files['error'][$position];
+
+        $file_ext = explode('.', $fileName);
+        $file_ext = strtolower(end($file_ext));
+
+        if (in_array($file_ext, $allowed)) {
+            if ($file_error === 0) {
+                if ($file_size <= 2097152) {
+                    $fileNameNew = uniqid('', true) . '.' . $file_ext;
+                    $file_destination = 'uploads/' . $fileNameNew;
+
+                    if (move_uploaded_file($file_tmp, $file_destination)) {
+                        $uploaded[$position] = $file_destination;
+                    } else {
+                        $failed[$position] = "[$fileName] failed to upload";
+                    }
+                } else {
+                    $failed[$position] = "[$fileName] is too large.";
+                }
+            } else {
+                $failed[$position] = "[$fileName] failed to uploaded - $file_error";
+            }   
+        } else {
+            $failed[$position] = "[$fileName] file extension '$file_ext' is not allowed";
+        }
+    }
+
+    if (!empty($uploaded)) {
+        var_dump($uploaded);
+    }
+
+    if (!empty($failed)) {
+        var_dump($failed);
+    }
+
+    header("Location: index.php");
+
+}
